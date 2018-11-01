d0b659a2578eb76c663fa74efbc4c417ef6d20ba
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-01 01:16:43 +0000

    Working to make cli more oo based

--- lib/my_anime_list/anime.rb
+++ lib/my_anime_list/anime.rb
@@ -8,31 +8,45 @@ class MyAnimeList::Anime
 
   attr_accessor :name, :show_length, :time_aired, :members_watched, :ranking, :url, :description, :genres
 
-  def self.today(anime_detailed_info)
-    anime_detailed_info.each do |anime_show_or_movie|
-      anime = self.new
-      anime.name = anime_show_or_movie[:name]
-      if anime.name.include? "Â°"
-        replace = anime.name.split("Â")
-        complete_item = replace[0] + replace[1]
-        anime.name = complete_item
-      end
+  def self.new_from_index_page(a)
 
-      anime.show_length = anime_show_or_movie[:show_length]
-      anime.time_aired = anime_show_or_movie[:time_aired]
-      anime.members_watched = anime_show_or_movie[:members_watched]
-      anime.url = anime_show_or_movie[:url]
+    anime_info = a.css("div.detail div.information.di-ib.mt4").text.strip
+    parsed_anime_info = anime_info.split(/\n/)
+    #     hash[:show_length] = parsed_info[0]
+    #     hash[:time_aired] = parsed_info[1].strip
+    #     hash[:members_watched] = parsed_info[2].strip
+    self.new(
+      a.css("div.detail div.di-ib.clearfix a.hoverinfo_trigger.fl-l.fs14.fw-b").text,
+      parsed_info[0],
+      parsed_info[1].strip,
+      parsed_info[2].strip
+    )
 
-      #for Gintama weird case
-      if anime.url.include? "Â°"
-        replace = anime.url.split("Â°")
-        complete_item = replace[0]
-        anime.url = complete_item
-      end
-
-      @@all << anime
-    end
-    @@all
-  end
+  # def self.today(anime_detailed_info)
+  #   anime_detailed_info.each do |anime_show_or_movie|
+  #     anime = self.new
+  #     anime.name = anime_show_or_movie[:name]
+  #     if anime.name.include? "Â°"
+  #       replace = anime.name.split("Â")
+  #       complete_item = replace[0] + replace[1]
+  #       anime.name = complete_item
+  #     end
+  #
+  #     anime.show_length = anime_show_or_movie[:show_length]
+  #     anime.time_aired = anime_show_or_movie[:time_aired]
+  #     anime.members_watched = anime_show_or_movie[:members_watched]
+  #     anime.url = anime_show_or_movie[:url]
+  #
+  #     #for Gintama weird case
+  #     if anime.url.include? "Â°"
+  #       replace = anime.url.split("Â°")
+  #       complete_item = replace[0]
+  #       anime.url = complete_item
+  #     end
+  #
+  #     @@all << anime
+  #   end
+  #   @@all
+  # end
 
 end
