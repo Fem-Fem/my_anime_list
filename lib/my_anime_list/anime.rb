d01199c9d0ff7cb6d30b5491df04d8d2819530a9
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-01 03:34:20 +0000

    Adding corner case

--- lib/my_anime_list/anime.rb
+++ lib/my_anime_list/anime.rb
@@ -12,6 +12,7 @@ class MyAnimeList::Anime
 
     anime_info = a.css("div.detail div.information.di-ib.mt4").text.strip
     parsed_anime_info = anime_info.split(/\n/)
+
     self.new(
       a.css("div.detail div.di-ib.clearfix a.hoverinfo_trigger.fl-l.fs14.fw-b").text,
       parsed_anime_info[0],
@@ -23,11 +24,23 @@ class MyAnimeList::Anime
 
   def initialize(name=nil, show_length=nil, time_aired=nil, members_watched=nil, url=nil)
     @name = name
-    @show_length = url
+    @show_length = show_length
     @time_aired = time_aired
     @members_watched = members_watched
     @url = url
+    if_gintama?
     @@all << self
+  end
+
+  def if_gintama?
+    if self.name.include? "Â°"
+      replace = self.name.split("Â")
+      complete_item = replace[0] + replace[1]
+      self.name = complete_item
+      replace = self.url.split("Â°")
+      complete_item = replace[0]
+      self.url = complete_item
+    end
   end
 
   def self.all
@@ -44,20 +57,13 @@ class MyAnimeList::Anime
 
   def genres
       genres = ""
-      rough_info = doc.css('div')[1].children.children.children.to_s
-      genres_rough_info = rough_info.split(/genres|.setCollapse/)
+      genres_rough_info = doc.css('div')[1].children.children.children.to_s.split(/genres|.setCollapse/)
       genres_rough_info.each do |example|
         if example[0..4] == "\", [\""
           genres = example
         end
       end
-      # make method?
-      binding.pry
-      genres = genres[4..200].gsub(/\",\"/, ", ")
-      genres = genres[1..200]
-      genres = genres[0..-4]
-      genres = genre
-      @genres = genres
+      @genres = genres[4..200].gsub(/\",\"/, ", ")[1..-4]
   end
 
   def description
@@ -68,11 +74,12 @@ class MyAnimeList::Anime
           if description.include? "\u2014"
             array = description.split(/\u2014/)
             word = ""
-            array.each do |no_dashes|
+            array.each do |phrase|
+              # binding.pry
               if word == ""
-                word = word + " " + no_dashes
+                word = word + " " + phrase
               else
-                word = word + " - " + no_dashes
+                word = word + " - " + phrase
               end
             end
             if summary == ""
